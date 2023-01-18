9f87cb9edddb61e95cdd2cc77208607a5eb82aab
Darko Stefanovic <darkost0502@gmail.com>
2023-01-19 00:03:21 +0100

    add player and monster win

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,25 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
+
+
 let chosenMaxLife = 100;
-let monsterHealth = 100;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 
 
 adjustHealthBars(chosenMaxLife);
 
-
-
 function attackHandler() {
-    const demage = dealMonsterDamage(ATTACK_VALUE)
+    const damage = dealMonsterDamage(ATTACK_VALUE)
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0) {
+        alert("Player win!!!")
+    } if (currentPlayerHealth <= 0) {
+        alert("Monster win!!!")
+
+    } 
+
 }
 attackBtn.addEventListener('click', attackHandler)
